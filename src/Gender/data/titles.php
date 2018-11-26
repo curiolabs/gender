3edf4402cf0d80c82ebe746b9691825c868e4463
CurioLabs <steven.p.wright@gmail.com>
2018-11-26 14:50:03 +0000

    Commit and push from PHPStorm

---
+++ src/Gender/data/titles.php
@@ -0,0 +1,74 @@
+<?php
+
+return array(
+
+    // en
+    'MR' => 'M',
+    'MASTER' => 'M',
+    'MS' => 'F',
+    'MRS' => 'F',
+    'MISS' => 'F',
+    'SIR' => 'M',
+    'LORD' => 'M',
+    'LADY' => 'F',
+    'BARON' => 'F',
+    'BARONESS' => 'F',
+
+    // es
+    'SENOR' => 'M',
+    'SENORA' => 'F',
+    'SENORITA' => 'F',
+    'DON' => 'M',
+    'DONA' => 'F',
+    'DOCTORA' => 'F',
+    'PROFESSORA' => 'F',
+
+    // dk
+    'FRU' => 'F',
+    '' => '',
+    // fi
+    'ROUVA' => 'F',
+    'RVA' => 'F',
+    'HERRA' => 'M',
+
+    // fr
+    'MADAME' => 'F',
+    'MME' => 'F',
+    'MADEMOISELLE' => 'F',
+    'MLLE' => 'F',
+    'MONSIEUR' => 'M',
+    '#M' => 'M',
+
+    // de
+    'FRAU' => 'F',
+    'FRAULEIN' => 'F',
+    'FRAU DOKTOR' => 'F',
+    'FR' => 'F',
+    'HERR' => 'M',
+    'HERR DOKTOR' => 'M',
+
+    // it
+    'SIG.RA' => 'F',
+    'SIGNORA' => 'F',
+    'SIGNOR' => 'M',
+    'SIG' => 'M',
+
+    // nl
+    'MEVROUW' => 'F',
+    'MEVR' => 'F',
+    'MW' => 'F',
+    'DE HEER' => 'M',
+
+    // pl
+    'PANI' => 'F',
+    '#P' => 'F',
+    'PAN' => 'M',
+
+    // pt
+    'SENHORA' => 'F',
+    'SRA' => 'F',
+    'SENHOR' => 'M',
+    'SR' => 'M',
+
+
+);
